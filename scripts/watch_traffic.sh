f44f079ce9db707c984e8b74c26a089c56ff7a17
Salaah Amin - Linux <salaah_amin@hotmail.co.uk>
2020-11-03 11:37:04 -0800

    script handles the use of env variables

--- scripts/watch_traffic.sh
+++ scripts/watch_traffic.sh
@@ -3,20 +3,33 @@
 
 usage() {
   echo "Usage ${0} [-pie]" >&2
-  echo "Watches the traffic by watching new entries stored into the access log." >&2
+  echo "Watches the traffic by watching new entries stored into the access " >&2
+  echo "log." >&2
   echo "  -p  ACCESS_LOG_PATH   Explicity specify the access log path or" >&2
   echo "                        store the path as an environment varable" >&2
-  echo "                        as ACCESS_LOG_PATH" >&2
-  echo "  -i  INCLUDE_PATTERN   Pattern to incude. Will be run with grep -iE" >&2
-  echo "  -e  EXCLUDE_PATTERN   Pattern to exclude. Will be run with grep -iEv" >&2
+  echo "                        as ${ACCESS_LOG_PATH}" >&2
+  echo "  -i  INCLUDE_PATTERN   Pattern to incude. Will be run with" >&2 
+  echo "                        grep -iE." >&2
+  echo "  -I                    Includes patterns that are set as an env" >&2
+  echo "                        var as ${ACCESS_LOG_INC_PATTERN}" >&2
+  echo "                        Seperate with ':'" >&2
+  echo "  -e  EXCLUDE_PATTERN   Pattern to exclude. Will be run with" >&2
+  echo "                        grep -iEv" >&2
+  echo "  -E                    Exclude patterns that are set as as an" >&2
+  echo "                        env var as ${ACCESS_LOG_EX_PATTERN}" >&2
+  echo "                        Seperate with ':'" >&2
   exit 1
 }
 
-ACCESS_LOG_PATH=$(printenv ACCESS_LOG_PATH)
+ACCESS_LOG_PATH_ENV_VAR='ACCESS_LOG_PATH'
+INC_PATTERN_ENV_VAR='ACCESS_LOG_INC_PATTERN'
+EXC_PATTERN_ENV_VAR='ACCESS_LOG_EX_PATTERN'
+
+ACCESS_LOG_PATH=$(printenv $ACCESS_LOG_PATH_ENV_VAR)
 INCLUDE_PATTERN=()
 EXCLUDE_PATTERN=()
 
-while getopts p:i:e: OPTION; do
+while getopts p:Ii:Ee: OPTION; do
   case ${OPTION} in
     p)
       ACCESS_LOG_PATH="${OPTARG}"
@@ -24,8 +37,27 @@ while getopts p:i:e: OPTION; do
     i)
       INCLUDE_PATTERN+=("${OPTARG}")
       ;;
+    I)
+      if [[ $(printenv $INC_PATTERN_ENV_VAR) == '' ]]; then
+        echo -e "\e[1;33m WARN: ${INC_PATTERN_ENV_VAR} is not defined\e[0m"
+      fi
+
+      IFS=':' read -ra PATTERNS <<< "$(printenv $INC_PATTERN_ENV_VAR)"
+      for pattern in "${PATTERNS[@]}"; do
+        INCLUDE_PATTERN+=("${pattern}")
+      done
+      ;;
     e)
       EXCLUDE_PATTERN+=("${OPTARG}")
+      ;;
+    E)
+    if [[ $(printenv $EXC_PATTERN_ENV_VAR) == '' ]]; then
+        echo -e "\e[1;33m WARN: ${EXC_PATTERN_ENV_VAR} is not defined\e[0m"
+      fi
+      IFS=':' read -ra PATTERNS <<< "$(printenv $EXC_PATTERN_ENV_VAR)"
+      for pattern in "${PATTERNS[@]}"; do
+        INCLUDE_PATTERN+=("${pattern}")
+      done
       ;;
     ?)
       usage
@@ -55,5 +87,13 @@ if [[ ${#EXCLUDE_PATTERN[@]} -gt 0 ]]; then
 fi
 
 # Run the watch command.
-echo "Running ${CMD}"
-$CMD
+# If the command contain '|', we would need to use the `eval` method to call
+# the script. This is not a safe method to use, so ask user to run it
+# themselves.
+if grep -q "|" <<< "$CMD"; then
+  echo -e "Run:\n${CMD}"
+else
+  echo -e "\e[1;32mRunning:\n${CMD}\e[0m"
+  ${CMD}
+fi
+
